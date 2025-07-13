e97f71180746e978aa18b2fd99a1b062eb89591e
neesan <exp.ym.lvl@gmail.com>
2025-07-13 23:38:52 +0900

    mockではなくinMemoryDataBaseを使う方針でテスト修正

--- Domain/src/test/java/com/neesan/domain/favorite/AddFavoriteVideoUseCaseTest.kt
+++ Domain/src/test/java/com/neesan/domain/favorite/AddFavoriteVideoUseCaseTest.kt
@@ -1,46 +1,65 @@
 package com.neesan.domain.favorite
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.neesan.core.runWithDescription
 import com.neesan.data.favorite.FavoriteRepository
 import com.neesan.data.favorite.FavoriteVideoEntity
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.HiltTestApplication
 import kotlinx.coroutines.test.runTest
+import org.junit.Assert
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
-import org.mockito.kotlin.mock
-import org.mockito.kotlin.verify
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+import javax.inject.Inject
 
+@HiltAndroidTest
+@Config(application = HiltTestApplication::class)
+@RunWith(AndroidJUnit4::class)
 @Suppress("NonAsciiCharacters", "TestFunctionName")
 class AddFavoriteVideoUseCaseTest {
 
-    private lateinit var favoriteRepository: FavoriteRepository
-    private lateinit var addFavoriteVideoUseCase: AddFavoriteVideoUseCase
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var favoriteRepository: FavoriteRepository
+
+    @Inject
+    lateinit var addFavoriteVideoUseCase: AddFavoriteVideoUseCase
 
     @Before
     fun setup() {
-        favoriteRepository = mock()
-        addFavoriteVideoUseCase = AddFavoriteVideoUseCase(favoriteRepository)
+        hiltRule.inject()
     }
 
     @Test
-    fun お気に入り動画が追加されること() = runTest {
-        // テストデータ
-        val favoriteVideoDomainData = FavoriteVideoDomainData(
-            videoId = "sm12345",
-            title = "テスト動画",
-            thumbnailUrl = "test_url",
-            createdAt = 1000L
-        )
+    fun お気に入り動画が追加されること() = runWithDescription {
+        runTest {
+            // テストデータ
+            val favoriteVideoDomainData = FavoriteVideoDomainData(
+                videoId = "sm12345",
+                title = "テスト動画",
+                thumbnailUrl = "test_url",
+                createdAt = 1000L
+            )
 
-        val expectedEntity = FavoriteVideoEntity(
-            videoId = "sm12345",
-            title = "テスト動画",
-            thumbnailUrl = "test_url",
-            createdAt = 1000L
-        )
+            // テスト実行
+            addFavoriteVideoUseCase.invoke(favoriteVideoDomainData)
 
-        // テスト実行
-        addFavoriteVideoUseCase.invoke(favoriteVideoDomainData)
-
-        // 検証
-        verify(favoriteRepository).addFavoriteVideo(expectedEntity)
+            // 検証
+            val expectedEntity = FavoriteVideoEntity(
+                videoId = "sm12345",
+                title = "テスト動画",
+                thumbnailUrl = "test_url",
+                createdAt = 1000L
+            )
+            favoriteRepository.getFavoriteVideoById("sm12345").collect { entity ->
+                Assert.assertTrue(entity == expectedEntity)
+            }
+        }
     }
 }
